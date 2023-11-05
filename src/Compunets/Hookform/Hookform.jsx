deb8fde5a8da31df5454dfd3409def802484b1da
Kamrul Hassan <kamrulthedev@gmail.com>
2023-11-05 18:29:37 +0600

    simple-Hooks-commit

--- src/Compunets/Hookform/Hookform.jsx
+++ src/Compunets/Hookform/Hookform.jsx
@@ -1,9 +1,22 @@
 
 
 const Hookform = () => {
+
+    const handlerSubmit = e =>{
+     e.preventDefault();
+    }
+
     return (
         <div>
-            
+             <form onSubmit={handlerSubmit}>
+                <input type="name" name="name" placeholder="Name"/>
+                <br />
+                <input type="email" name="email" placeholder="Email"/>
+                <br />
+                <input type="password" name="password" id="" placeholder="password" />
+                <br/>
+                 <input type="submit" value="Submit" />
+            </form>
         </div>
     );
 };
