5b9ffae4eed9d5786ead1f73cde228ae8be4133d
Kamrul Hassan <kamrulthedev@gmail.com>
2023-11-06 09:22:13 +0600

    tik-ace-commit

--- src/Hooks/useInputState.js
+++ src/Hooks/useInputState.js
@@ -1,20 +1,36 @@
+// import { useState } from "react"
+
 import { useState } from "react"
 
-const useInputState = (defaultValo = null) =>{
-   const [input, setinput] = useState(defaultValo);
+// const useInputState = (defaultValo = null) =>{
+//    const [input, setinput] = useState(defaultValo);
 
-    // const handleChange = e =>{
-    //  setinput(e.target.value)
-    // }
-    // return[input, handleChange];
+//     // const handleChange = e =>{
+//     //  setinput(e.target.value)
+//     // }
+//     // return[input, handleChange];
 
-    const onChange = e =>{
-        setinput(e.target.value)
-       }
-       return{
-        input,
-        onChange
-       }
+//     const onChange = e =>{
+//         setinput(e.target.value)
+//        }
+//        return{
+//         input,
+//         onChange
+//        }
+// }
+
+// export default useInputState;
+
+
+const useInputState = (defaultValue = null)=>{
+    const [input, setinput] = useState(defaultValue);
+
+    const onChange= e=>{
+           setinput(e.target.value)
+    }
+    return{
+        input, onChange
+    }
 }
 
-export default useInputState;
+export default useInputState
